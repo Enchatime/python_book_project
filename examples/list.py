dbd0290544f518416ec42a6d923d2d6f293a681f
Artem <enchatime@icloud.com>
2020-04-08 11:56:02 +0300

    fix list.py

--- examples/list.py
+++ examples/list.py
@@ -6,7 +6,7 @@ found = []
 for letter in word:
     if letter in letters:
         found.append(letter)  # add to the end of list
-        found.remove("t")     # remove from list by string
+        found.remove("s")     # remove from list by string
         found.pop(0)          # remove from list by index (starts from zero)
         found.extend([3, 4])  # add list to the end of list
         found.insert(0, "s")  # add to the start of list (sets index)
