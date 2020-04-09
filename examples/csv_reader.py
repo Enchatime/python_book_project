a9d09f45805c64fa05f14a8c3452f675b6952ac9
Artem <enchatime@icloud.com>
2020-04-09 20:06:30 +0300

    new regexp

--- examples/csv_reader.py
+++ examples/csv_reader.py
@@ -1,14 +1,10 @@
 import csv
 import re
 
-regex1 = '"8.'
-regex2 = '"Y'
-regex3 = '"P'
+regex1 = '"8.+"Y.+"F.'
 
-with open('E:\test_csv\test_csv.csv', newline='') as csvfile:
+with open('E:\\test_csv\\test_csv.csv', newline='') as csvfile:
      numbers = csv.reader(csvfile, delimiter=';', quotechar='|')
      for row in numbers:
-        if re.match(regex1, str(row[2])):
-            if re.match(regex2, str(row[3])):
-                if re.match(regex3, str(row[4])):
-                    print(row)
+        if re.findall(regex1, str(row)):
+            print(row[2])
