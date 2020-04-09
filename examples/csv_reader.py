17fb56503cc9eb869bd90b1972bfd4a85aea751b
Artem <enchatime@icloud.com>
2020-04-09 16:50:04 +0300

    find by regex pattern

--- examples/csv_reader.py
+++ examples/csv_reader.py
@@ -1,6 +1,14 @@
 import csv
+import re
 
-with open('E:\csv_files\test.csv', newline='') as csvfile:
+regex1 = '"8.'
+regex2 = '"Y'
+regex3 = '"P'
+
+with open('E:\test_csv\test_csv.csv', newline='') as csvfile:
      numbers = csv.reader(csvfile, delimiter=';', quotechar='|')
      for row in numbers:
-         print(', '.join(row))
+        if re.match(regex1, str(row[2])):
+            if re.match(regex2, str(row[3])):
+                if re.match(regex3, str(row[4])):
+                    print(row)
