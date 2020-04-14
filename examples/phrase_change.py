36e8ea55d8d4483d66820ed6bd8b5080f7ec01ce
Artem <enchatime@icloud.com>
2020-04-14 20:32:12 +0300

    new actions with list

---
+++ examples/phrase_change.py
@@ -0,0 +1,16 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+
+print(phrase)
+print(plist)
+
+index = ["D", "'", "i", "c", "!"]
+
+for i in index:
+    plist.remove(i)
+
+plist.pop(-1)
+plist.insert(2, ' ')
+plist.extend([plist.pop(), plist.pop()])
+plist.pop(4)
+print(plist)
