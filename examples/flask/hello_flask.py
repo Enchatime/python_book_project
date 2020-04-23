56b2d8ae8ad42aa80cd39c67f8aecd9348a579f5
Artem <enchatime@icloud.com>
2020-04-23 18:31:38 +0300

    added html templates for flask example

--- examples/flask/hello_flask.py
+++ examples/flask/hello_flask.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request #redirect - module for redirect
+from examples.def_ex import search4letters
 
 app = Flask(__name__)
 
+
+@app.route('/search4', methods=['POST'])
+def search() -> str:
+    title = 'Here are you results:'
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                           the_results=results,
+                           the_letters=letters,
+                           the_title=title,
+                           the_phrase=phrase,)
+
+
 @app.route('/')
-def hello() -> str:
-    return 'Hello from Flask'
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
