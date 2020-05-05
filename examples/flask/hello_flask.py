6f2df4476fd0eaf5fa8f4d861b413a9ea38e4d96
Artem <enchatime@icloud.com>
2020-05-05 23:33:28 +0300

    modified hello_flask:

--- examples/flask/hello_flask.py
+++ examples/flask/hello_flask.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request #redirect - module for redirect to another url
+from flask import Flask, render_template, request, escape #redirect - module for redirect to another url
 from examples.def_ex import search4letters
 
 app = Flask(__name__)
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as logs:
+        print(req.form, req.remote_addr, req.user_agent, res, file=logs, sep="|")
 
 
 @app.route('/search4', methods=['POST'])
@@ -10,6 +15,7 @@ def search() -> str:
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                            the_results=results,
                            the_letters=letters,
@@ -21,6 +27,13 @@ def search() -> str:
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome')
+
+
+@app.route('/viewlog')
+def view_logs() -> str:
+    with open('vsearch.log', 'r') as log:
+        contents = log.read()
+    return escape(contents)
 
 
 if __name__ == '__main__':
